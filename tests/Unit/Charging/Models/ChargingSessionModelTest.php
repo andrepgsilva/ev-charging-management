b105ade77a5c579fcad2afcad295b83df03580dd
andre silva <andrepgsilva@outlook.com>
2025-05-28 04:43:03 +0100

    feat: Add `ChargingSessionModelTest`, enforce unique rules in `UpdateCompanyRequest`, and remove `.env.testing` file

---
+++ tests/Unit/Charging/Models/ChargingSessionModelTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Modules\Charging\Models\ChargingSession;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+uses(DatabaseMigrations::class);
+
+test('to array', function () {
+    $chargingSession = ChargingSession::factory()->createOne();
+
+    expect(array_keys($chargingSession->toArray()))
+        ->toEqual([
+            'company_id',
+            'vehicle_id',
+            'driver_id',
+            'location_id',
+            'start_time',
+            'end_time',
+            'energy_kwh',
+            'status',
+            'updated_at',
+            'created_at',
+            'id',
+        ]);
+});
